58c67dccdfd6737e193df136de6a07630d0a5de3
pradeepyadav5137 <pradeepapex27@gmail.com>
2023-09-03 19:01:54 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+// import capcha from './capcha'
+import Tesla from './App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <Tesla/>
+  </React.StrictMode>,
+)
+
